f3ad97ed7d768487b0e4fc7d1970aa7e5fd6079e
njtsb1 <njtsb1@gmail.com>
2024-11-29 12:00:14 -0300

    Starting work

---
+++ README.md
@@ -0,0 +1,10 @@
+Daily Learning
+
+# Creating a Blog with Angular
+
+Project developed at the Bootcamp NTT DATA Diversity in Tech with guidance from specialist [Felipe Aguiar](https://github.com/felipeAguiarCode/ "Felipe Aguiar")
+Learning how to create a functional blog application, building intelligent components and learning how to organize the Angular project using the tools that the framework itself offers.
+
+[LICENSE](/LICENSE)
+
+See [original repository](https://github.com/felipeAguiarCode/angular-blog)
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NEW 3D IRON MAN",
+    "description": "Marvel announces a new Iron Man film, check it out",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"2",
+    "title": "New Series Announced on Disney+",
+    "description": "New Series Announced on Disney+, check it out",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"3",
+    "title": "New Black Panther movie coming soon",
+    "description": "New Black Panther movie coming soon, check it out",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=1200&aspectRatio=1.78&format=jpeg",
+  },
+  {
+    "id":"4",
+    "title": "New X-Men film is in post-production",
+    "description": "New X-Men film is in post-production, says director",
+    "photoCover":"https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339",
+  }
+]
